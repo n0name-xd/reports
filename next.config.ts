9e7b6c03998b75607f9ba019409da36441aa09d4
Evgeny_Grinchuk <n0name-xd@inbox.ru>
2025-12-10 20:37:01 +0300

    fix: config

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
-  basePath: "https://github.com/n0name-xd/reports",
+  basePath: process.env.PAGES_BASE_PATH,
 };
 
 export default nextConfig;
